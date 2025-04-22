ccb3505fa6cad9512d7f7482407a9d19ed7a821c
ASUS <faris13abiyu@gmail.com>
2025-04-23 02:53:35 +0700

    Add project files.

---
+++ ManagerSistemParkir/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace ManagerSistemParkir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtKendaraan = new System.Windows.Forms.TextBox();
+            this.txtPlat = new System.Windows.Forms.TextBox();
+            this.txtMasuk = new System.Windows.Forms.TextBox();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnUbah = new System.Windows.Forms.Button();
+            this.dgvDataParkir = new System.Windows.Forms.DataGridView();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtKeluar = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDataParkir)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Jenis Kendaraan";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Plat Nomor";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(80, 158);
+            this.Label3.Name = "Label3";
+            this.Label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Label3.Size = new System.Drawing.Size(61, 13);
+            this.Label3.TabIndex = 2;
+            this.Label3.Text = "Jam Masuk";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(316, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Data Kendaraan parkir Basement";
+            // 
+            // txtKendaraan
+            // 
+            this.txtKendaraan.Location = new System.Drawing.Point(192, 65);
+            this.txtKendaraan.Name = "txtKendaraan";
+            this.txtKendaraan.Size = new System.Drawing.Size(193, 20);
+            this.txtKendaraan.TabIndex = 4;
+            // 
+            // txtPlat
+            // 
+            this.txtPlat.Location = new System.Drawing.Point(192, 107);
+            this.txtPlat.Name = "txtPlat";
+            this.txtPlat.Size = new System.Drawing.Size(193, 20);
+            this.txtPlat.TabIndex = 5;
+            // 
+            // txtMasuk
+            // 
+            this.txtMasuk.Location = new System.Drawing.Point(192, 155);
+            this.txtMasuk.Name = "txtMasuk";
+            this.txtMasuk.Size = new System.Drawing.Size(193, 20);
+            this.txtMasuk.TabIndex = 6;
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Location = new System.Drawing.Point(416, 63);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(75, 23);
+            this.btnTambah.TabIndex = 7;
+            this.btnTambah.Text = "Tambah";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            this.btnTambah.Click += new System.EventHandler(this.btnTambah_Click_1);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(416, 104);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(75, 23);
+            this.btnHapus.TabIndex = 8;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click_1);
+            // 
+            // btnUbah
+            // 
+            this.btnUbah.Location = new System.Drawing.Point(416, 153);
+            this.btnUbah.Name = "btnUbah";
+            this.btnUbah.Size = new System.Drawing.Size(75, 23);
+            this.btnUbah.TabIndex = 9;
+            this.btnUbah.Text = "Ubah";
+            this.btnUbah.UseVisualStyleBackColor = true;
+            this.btnUbah.Click += new System.EventHandler(this.btnUbah_Click);
+            // 
+            // dgvDataParkir
+            // 
+            this.dgvDataParkir.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDataParkir.Location = new System.Drawing.Point(80, 257);
+            this.dgvDataParkir.Name = "dgvDataParkir";
+            this.dgvDataParkir.Size = new System.Drawing.Size(623, 209);
+            this.dgvDataParkir.TabIndex = 10;
+            this.dgvDataParkir.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDataParkir_CellContentClick);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(416, 204);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 11;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(79, 206);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Jam Keluar";
+            // 
+            // txtKeluar
+            // 
+            this.txtKeluar.Location = new System.Drawing.Point(192, 206);
+            this.txtKeluar.Name = "txtKeluar";
+            this.txtKeluar.Size = new System.Drawing.Size(193, 20);
+            this.txtKeluar.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 478);
+            this.Controls.Add(this.txtKeluar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.dgvDataParkir);
+            this.Controls.Add(this.btnUbah);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.txtMasuk);
+            this.Controls.Add(this.txtPlat);
+            this.Controls.Add(this.txtKendaraan);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDataParkir)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Label3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtKendaraan;
+        private System.Windows.Forms.TextBox txtPlat;
+        private System.Windows.Forms.TextBox txtMasuk;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnUbah;
+        private System.Windows.Forms.DataGridView dgvDataParkir;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtKeluar;
+    }
+}
+
---
+++ ManagerSistemParkir/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManagerSistemParkir
+{
+    public partial class Form1 : Form
+    {
+        // Perbaikan nama database: ManajemenParkir2 (sesuai dengan script SQL)
+        private string connectionString = "Data Source=LAPTOP-JICJ6MBI\\FARISNAUFAL;Initial Catalog=ManajemenParkir2;Integrated Security=True;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void ClearForm()
+        {
+            txtKendaraan.Clear();
+            txtPlat.Clear();
+            txtMasuk.Clear();
+            txtKeluar.Clear();
+
+            txtPlat.Focus();
+        }
+
+        private void LoadData()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT id_kendaraan, plat_nomor, jenis_kendaraan, waktu_masuk, waktu_keluar FROM kendaraan";
+                    SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    dgvDataParkir.AutoGenerateColumns = true;
+                    dgvDataParkir.DataSource = dt;
+
+                    // Menyembunyikan kolom ID jika diperlukan
+                    if (dgvDataParkir.Columns.Contains("id_kendaraan"))
+                    {
+                        dgvDataParkir.Columns["id_kendaraan"].Visible = false;
+                    }
+
+                    ClearForm();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnTambah_Click_1(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    if (string.IsNullOrEmpty(txtPlat.Text) || string.IsNullOrEmpty(txtKendaraan.Text) || string.IsNullOrEmpty(txtMasuk.Text))
+                    {
+                        MessageBox.Show("Semua data harus diisi", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    // Validasi jenis kendaraan
+                    string jenisKendaraan = txtKendaraan.Text.Trim();
+                    if (jenisKendaraan != "Motor" && jenisKendaraan != "Mobil")
+                    {
+                        MessageBox.Show("Jenis kendaraan harus 'Motor' atau 'Mobil'", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    // Validasi format tanggal
+                    DateTime waktuMasuk;
+                    if (!DateTime.TryParse(txtMasuk.Text.Trim(), out waktuMasuk))
+                    {
+                        MessageBox.Show("Format waktu masuk tidak valid. Gunakan format tanggal yang benar", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    conn.Open();
+                    string query = "INSERT INTO kendaraan (plat_nomor, jenis_kendaraan, waktu_masuk) VALUES (@plat_nomor, @jenis_kendaraan, @waktu_masuk)";
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@plat_nomor", txtPlat.Text.Trim());
+                        cmd.Parameters.AddWithValue("@jenis_kendaraan", jenisKendaraan);
+                        cmd.Parameters.AddWithValue("@waktu_masuk", waktuMasuk);
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Data berhasil ditambahkan", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            LoadData();
+                            ClearForm();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Data gagal ditambahkan", "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnHapus_Click_1(object sender, EventArgs e)
+        {
+            if (dgvDataParkir.SelectedRows.Count > 0)
+            {
+                DialogResult confirm = MessageBox.Show("Apakah Anda yakin ingin menghapus data ini?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (confirm == DialogResult.Yes)
+                {
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        try
+                        {
+                            // Perbaikan: Menggunakan nama kolom yang benar dan tabel yang benar
+                            int idKendaraan = Convert.ToInt32(dgvDataParkir.SelectedRows[0].Cells["id_kendaraan"].Value);
+
+                            conn.Open();
+                            // Perbaikan: Query DELETE untuk tabel kendaraan dan parameter yang benar
+                            string query = "DELETE FROM kendaraan WHERE id_kendaraan = @id_kendaraan";
+
+                            using (SqlCommand cmd = new SqlCommand(query, conn))
+                            {
+                                cmd.Parameters.AddWithValue("@id_kendaraan", idKendaraan);
+                                int rowsAffected = cmd.ExecuteNonQuery();
+
+                                if (rowsAffected > 0)
+                                {
+                                    MessageBox.Show("Data berhasil dihapus", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                    LoadData();
+                                    ClearForm();
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Data gagal dihapus", "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error: " + ex.Message, "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Pilih data yang akan dihapus", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnRefresh_Click_1(object sender, EventArgs e)
+        {
+            LoadData();
+
+            MessageBox.Show($"Jumlah kolom : {dgvDataParkir.ColumnCount}\nJumlah Baris : {dgvDataParkir.RowCount}",
+                "Debugging DataGridView", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void dgvDataParkir_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvDataParkir.Rows[e.RowIndex];
+
+                txtPlat.Text = row.Cells["plat_nomor"].Value.ToString();
+                txtKendaraan.Text = row.Cells["jenis_kendaraan"].Value.ToString();
+
+                if (row.Cells["waktu_masuk"].Value != DBNull.Value)
+                {
+                    DateTime waktuMasuk = Convert.ToDateTime(row.Cells["waktu_masuk"].Value);
+                    txtMasuk.Text = waktuMasuk.ToString("yyyy-MM-dd HH:mm:ss");
+                }
+                else
+                {
+                    txtMasuk.Text = string.Empty;
+                }
+
+                if (row.Cells["waktu_keluar"].Value != DBNull.Value)
+                {
+                    DateTime waktuKeluar = Convert.ToDateTime(row.Cells["waktu_keluar"].Value);
+                    txtKeluar.Text = waktuKeluar.ToString("yyyy-MM-dd HH:mm:ss");
+                }
+                else
+                {
+                    txtKeluar.Text = string.Empty;
+                }
+            }
+        }
+
+        private void btnUbah_Click(object sender, EventArgs e)
+        {
+            if (dgvDataParkir.SelectedRows.Count > 0)
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    try
+                    {
+                        // Validasi input
+                        if (string.IsNullOrEmpty(txtPlat.Text) || string.IsNullOrEmpty(txtKendaraan.Text) || string.IsNullOrEmpty(txtMasuk.Text))
+                        {
+                            MessageBox.Show("Semua data harus diisi", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            return;
+                        }
+
+                        // Validasi jenis kendaraan
+                        string jenisKendaraan = txtKendaraan.Text.Trim();
+                        if (jenisKendaraan != "Motor" && jenisKendaraan != "Mobil")
+                        {
+                            MessageBox.Show("Jenis kendaraan harus 'Motor' atau 'Mobil'", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            return;
+                        }
+
+                        // Validasi format tanggal
+                        DateTime waktuMasuk;
+                        if (!DateTime.TryParse(txtMasuk.Text.Trim(), out waktuMasuk))
+                        {
+                            MessageBox.Show("Format waktu masuk tidak valid. Gunakan format tanggal yang benar (yyyy-MM-dd HH:mm:ss)", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            return;
+                        }
+
+                        // Dapatkan ID kendaraan dari baris yang dipilih
+                        int idKendaraan = Convert.ToInt32(dgvDataParkir.SelectedRows[0].Cells["id_kendaraan"].Value);
+
+                        conn.Open();
+                        string query = "UPDATE kendaraan SET plat_nomor = @plat_nomor, jenis_kendaraan = @jenis_kendaraan, waktu_masuk = @waktu_masuk WHERE id_kendaraan = @id_kendaraan";
+
+                        using (SqlCommand cmd = new SqlCommand(query, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@id_kendaraan", idKendaraan);
+                            cmd.Parameters.AddWithValue("@plat_nomor", txtPlat.Text.Trim());
+                            cmd.Parameters.AddWithValue("@jenis_kendaraan", jenisKendaraan);
+                            cmd.Parameters.AddWithValue("@waktu_masuk", waktuMasuk);
+
+                            int rowsAffected = cmd.ExecuteNonQuery();
+                            if (rowsAffected > 0)
+                            {
+                                MessageBox.Show("Data berhasil diubah", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                LoadData();
+                                ClearForm();
+                            }
+                            else
+                            {
+                                MessageBox.Show("Data gagal diubah", "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message, "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Pilih data yang akan diubah", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
